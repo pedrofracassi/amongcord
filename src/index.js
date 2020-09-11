02b27fbb6c8b1aea8b9671d03bfc2b2969fb8d3a
Lucca Nunes <luccanunesgit@gmail.com>
2020-09-11 00:12:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
+let prefix = ","; // get from db or whatever
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on('message', message => {
-  if (message.author.id === client.user.id) return
+client.on("message", (message) => {
+    if (message.author.id === client.user.id) return;
+    function setMuteState(bool) {
+        message.member.voice.channel.members.forEach((m) => {
+            if (m.user.bot) return;
+            m.voice.setMute(bool).catch(console.log);
+        });
+    }
+    switch (message.content) {
+        case `${prefix}mute`:
+            setMuteState(true);
+            break;
+        case `${prefix}unmute`:
+            setMuteState(false);
+            break;
+    }
+});
 
-  if (message.content === ',mute') {
-    message.member.voice.channel.members.forEach(m => {
-      if (m.user.bot) return
-      m.voice.setMute(true).catch(console.log)
-    })
-  }
-
-  if (message.content === ',unmute') {
-    message.member.voice.channel.members.forEach(m => {
-      if (m.user.bot) return
-      m.voice.setMute(false).catch(console.log)
-    })
-  }
-})
-
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
