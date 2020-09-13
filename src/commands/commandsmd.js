f1ac129a0a818876f9fa1d911c8126a9792c78b5
pedrofracassi <pedroluizfracassi@gmail.com>
2020-09-13 18:48:59 -0300

    Add commandsmd command

---
+++ src/commands/commandsmd.js
@@ -0,0 +1,17 @@
+const Command = require('../Command')
+const { MessageEmbed } = require('discord.js')
+
+
+module.exports = class CommandsMD extends Command {
+  constructor () {
+    super({
+      name: 'commandsmd',
+      description: 'Command list markdown',
+      hidden: true
+    })
+  }
+
+  run ({ message, commands, prefix }) {
+    message.channel.send(`\`\`\`| Command | Aliases | Description |\n|-|-|-|\n${commands.filter(c => !c.hidden).map(c => `| \`${prefix}${c.name}${c.usage ? ` ${c.usage}` : ''}\` | ${c.aliases ? c.aliases.map(a => `\`${prefix}${a}\``).join(',') : ''} | ${c.description} |`).join('\n')}\`\`\``)
+  }
+}
